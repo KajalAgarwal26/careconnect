e1d94f8223c29aeca57903e8a5f1372a30b1ece5
kavisankar90@gmail.com <vishalimoorthy1998@gmail.com>
2020-02-12 15:34:08 +0530

    Commit

--- src/main/java/com/ing/careconnect/service/UserServiceImpl.java
+++ src/main/java/com/ing/careconnect/service/UserServiceImpl.java
@@ -2,10 +2,13 @@ package com.ing.careconnect.service;
 
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ing.careconnect.constant.CareConnectConstant;
+import com.ing.careconnect.controller.UserController;
 import com.ing.careconnect.dto.LoginDto;
 import com.ing.careconnect.dto.LoginResponseDto;
 import com.ing.careconnect.entity.Doctors;
@@ -14,8 +17,17 @@ import com.ing.careconnect.exception.UserNotFoundException;
 import com.ing.careconnect.repository.DoctorRepository;
 import com.ing.careconnect.repository.UserRepository;
 
+/**
+ * @author Shankar
+ * 
+ *         This class used to check login weather the user is patient or doctor
+ * 
+ */
+
 @Service
 public class UserServiceImpl implements UserService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	UserRepository userRepository;
@@ -23,10 +35,21 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	DoctorRepository doctorRepository;
 
+	/**
+	 * @author Shankar
+	 *
+	 *         Method is used to check the input Login and if login success means it
+	 *         will return success else it will throw Invalid User
+	 *
+	 * @param LoginDto
+	 * @return LoginResponseDto
+	 */
+
 	@Override
 	public LoginResponseDto usersLogin(LoginDto userDto) {
 		Users user = userRepository.findByMobileAndPassword(userDto.getMobile(), userDto.getPassword());
 		Doctors doctor = new Doctors();
+		LOGGER.debug("Inside UserService :: usersLogin");
 		if (!Objects.isNull(user)) {
 			if (user.getType().equalsIgnoreCase(CareConnectConstant.DOCTOR_TYPE)) {
 				doctor = doctorRepository.findByUserId(user.getUserId());
@@ -40,5 +63,4 @@ public class UserServiceImpl implements UserService {
 		responseDto.setDoctorId(doctor.getDoctorId());
 		return responseDto;
 	}
-
 }
