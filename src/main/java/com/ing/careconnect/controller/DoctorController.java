dc1befe7f8d3e1304f2ddb1a1fcd9ea737a6f715
Rajesh Bojja <bojja.r@HCL.COM>
2020-02-12 16:09:40 +0530

    comments added for doctors controller

--- src/main/java/com/ing/careconnect/controller/DoctorController.java
+++ src/main/java/com/ing/careconnect/controller/DoctorController.java
@@ -28,18 +28,36 @@ public class DoctorController {
 	
 	@Autowired
 	DoctorService doctorService;
+	
+	/**
+	 * API to get the doctors info by doctorId 
+	 * 
+	 * @param 	doctorId
+	 * @return 	DoctorsResponseDto
+	 */
 	@GetMapping("/bookedslots/{doctorId}")
 	public ResponseEntity<DoctorsResponseDto> getBookedSlots(@PathVariable("doctorId") Long doctorId)
 	{
 		DoctorsResponseDto doctorResp = doctorService.getBookedSlots(doctorId);
 		return new ResponseEntity<>(doctorResp,HttpStatus.OK);
 	}
+	/**
+	 * API to get the list of doctors 
+	 * 
+	 * @return 	AllDoctorsDTO contains list of doctors
+	 */
 	@GetMapping("/allDoctors")
 	public ResponseEntity<AllDoctorsDTO> getAllDoctors()
 	{
 		AllDoctorsDTO searchResponse = doctorService.getAllDoctors();
 		return new ResponseEntity<>(searchResponse,HttpStatus.OK);
 	}
+	/**
+	 * API to show the search results by passing categery location specialist
+	 * 
+	 * @param 	doctorId
+	 * @return 	DoctorsResponseDto
+	 */
 	@GetMapping("/searchdoctors/{location}/{categery}/{specialist}")
 	public ResponseEntity<List<SearchResponseDto>> getAllDoctorsBySearch(@PathVariable("location") String location,@PathVariable("categery") String categery,@PathVariable("specialist") String specialist )
 	{
